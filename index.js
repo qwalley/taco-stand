f9244ff5c5626941d89f7903c8c671a5d0d58a11
qwalley <willalley345@gmail.com>
2021-03-09 14:58:29 -0500

    refactoring to use async/await instead of then

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const mongoose = require('mongoose');
 const Taco = require('./models/Taco');
 const uri = require('./crabby_patty_recipe.json').mongo_uri;
 
+// define Async Wrapper that catches errors throw by 'await's and passes them to Express handler
+function aw (callback) {
+	return function (req, res, next) {
+		callback(req, res, next).catch(next);
+	}
+}
+
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 	.then(_ => console.log('Database connected'))
 	.catch(error => console.error('Error making connection: ', error));
@@ -24,46 +31,34 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // GET
-app.get('/', (req, res) => {
-	Taco.find()
-		.then(results => { 
-			res.render('index.ejs', { tacos: results });
-		})
-		.catch(error => console.error(error));
-});
+app.get('/', aw(async (req, res) => {
+	const results = await Taco.find();
+	res.render('index.ejs', { tacos: results });
+}));
 
 // POST
-app.post('/tacos', (req, res) => {
-	Taco.create(req.body)
-		.then(doc => {
-			res.json('Success');
-		})
-		.catch(error => console.error(error));
-});
+app.post('/tacos', aw(async (req, res) => {
+	const result = await Taco.create(req.body)
+	res.json('Success');		
+}));
 
 // PUT
-app.put('/tacos', (req, res) => {
-	Taco.findOneAndUpdate(
+app.put('/tacos', aw(async (req, res, next) => {
+	const result = await Taco.findOneAndUpdate(
 		{ name: { $not: { $eq: 'Edgar' } } },
 		{ $set: req.body },
 		{ upsert: true }
-	)
-		.then(result => { 
-			res.json('Success');
-		})
-		.catch(error => console.error(error));
-});
+	);
+	res.json('Success');
+}));
 
 // DELETE
-app.delete('/tacos', (req, res) => {
-	Taco.deleteOne({ name: req.body.name })
-		.then(result => { 
-			if (result.deletedCount === 0) {
-				return res.json('No poop to clean up');
-			}
-			res.json('Cleaned up Edgar\'s poop');
-		})
-		.catch(error => console.error(error));
-});
+app.delete('/tacos', aw(async (req, res, next) => {
+	const result = await Taco.deleteOne({ name: req.body.name })
+	if (result.deletedCount === 0) {
+		return res.json('No poop to clean up');
+	}
+	res.json('Cleaned up Edgar\'s poop');	
+}));
 
 app.listen(3001, () => { console.log('listening on 3001') });
